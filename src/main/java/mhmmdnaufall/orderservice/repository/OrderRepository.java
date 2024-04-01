61e307a37c55987e0a7bf0390ba987228b5509c0
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-04-01 22:57:44 +0700

    Create OrderRepository

---
+++ src/main/java/mhmmdnaufall/orderservice/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package mhmmdnaufall.orderservice.repository;
+
+import mhmmdnaufall.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> { }
